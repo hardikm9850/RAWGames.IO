395bd6de66364817bf913320e532b581dceabf72
Hardik.Mehta <hardikm9850@gmail.com>
2026-04-02 09:49:27 +0530

    feat : favourite games screen created

---
+++ RAWGames/Features/Main/MainTabView.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabView.swift
+//  RAWGames
+//
+//  Created by Hardik Mehta on 31/03/26.
+//
+import SwiftUI
+import CoreData
+
+struct MainTabView: View {
+    @Environment(DependencyContainer.self) private var container
+    
+    var body: some View {
+        TabView {
+            GamesListView(
+                viewModel: container.gamesListViewModel
+            )
+            .tabItem {
+                Image(systemName: "house")
+                Text("Random Games")
+            }
+            
+            FavouritesView(
+                viewModel: container.favouritesViewModel
+            )
+            .tabItem {
+                Image(systemName: "heart.fill")
+                Text("Favorites")
+            }
+            
+            NotesView()
+            .tabItem {
+                Image(systemName: "square.and.pencil")
+                Text("Notes")
+            }
+        }
+    }
+}
---
+++ RAWGames/Features/Notes/Presentation/NotesView.swift
@@ -0,0 +1,14 @@
+//
+//  NotesView.swift
+//  RAWGames
+//
+//  Created by Hardik Mehta on 01/04/26.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Spacer()
+    }
+}
