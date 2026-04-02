50602227f97bf721361d5a2b6f9bc2a1933e26d6
Hardik.Mehta <hardikm9850@gmail.com>
2026-04-02 07:54:47 +0530

    feat : generated core data entities for game list

---
+++ RAWGames/Core/CoreData/CachedGame+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CachedGame+CoreDataClass.swift
+//  RAWGames
+//
+//  Created by Hardik Mehta on 01/04/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias CachedGameCoreDataClassSet = NSSet
+
+@objc(CachedGame)
+public class CachedGame: NSManagedObject {
+
+}
