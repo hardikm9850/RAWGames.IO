58c45ade8bcaa97d5a3e3fe7816b9f7ad427c830
Hardik.Mehta <hardikm9850@gmail.com>
2026-04-13 09:55:29 +0530

    add : date formatter

--- RAWGames/Core/Utils/DateUtils.swift
+++ RAWGames/Core/Utils/DateUtils.swift
@@ -10,13 +10,19 @@ import Foundation
 func formattedDate(_ date: String?) -> String {
     guard let date else { return "-" }
     
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd"
+    let inputFormatter = DateFormatter()
+    inputFormatter.locale = Locale(identifier: "en_US_POSIX")
+    inputFormatter.calendar = Calendar(identifier: .gregorian)
+    inputFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    inputFormatter.dateFormat = "yyyy-MM-dd"
     
-    if let d = formatter.date(from: date) {
-        formatter.dateFormat = "MMM d, yyyy"
-        return formatter.string(from: d)
+    if let d = inputFormatter.date(from: date) {
+        let outputFormatter = DateFormatter()
+        outputFormatter.locale = .current
+        outputFormatter.calendar = Calendar(identifier: .gregorian)
+        outputFormatter.timeZone = .current
+        outputFormatter.dateFormat = "MMM d, yyyy"
+        return outputFormatter.string(from: d)
     }
-    
     return date
 }
